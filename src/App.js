3d427e5dac1f173ebd2715e5b71cfcf250f2f83c
nik frank <nikbfrank@gmail.com>
2019-01-30 13:46:31 -0800

    render hand

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { Hand } from 'react-deck-o-cards';
+
 class App extends Component {
+  state = {
+    cards: [
+      { rank: 1, suit: 3 },
+      { rank: 13, suit: 3 },
+    ],
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Hand cards={this.state.cards}/>
       </div>
     );
   }
