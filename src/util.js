33a64db8f550cf0ee238c6e140007cb6e752d790
nik frank <nikbfrank@gmail.com>
2019-01-30 14:29:02 -0800

    dealer status and test

--- src/util.js
+++ src/util.js
@@ -12,3 +12,17 @@ export const handStatus = cards => {
             'live',
   };
 };
+
+export const dealerStatus = cards=>{
+  const hasAce = !!cards.find(c => c.rank === 1);
+  const total = cards.reduce((p, c)=> p+ Math.min(10, c.rank), 0);
+
+  return {
+    total: hasAce && total <= 11 ? total + 10 : total,
+    status: (total > 21) ? 'bust' :
+            (hasAce && (total === 21)) ? 'blackjack' :
+            (hasAce && (total >= 18)) ? 'standing' : // dealer hits on soft 17 lo
+            (!hasAce && (total >= 17)) ? 'standing' :
+            'hitting',
+  };
+}
--- src/util.test.js
+++ src/util.test.js
@@ -1,4 +1,4 @@
-import { handStatus } from '../src/util';
+import { handStatus, dealerStatus } from '../src/util';
 
 it('should calculate bust blackjack or live hand status', ()=>{
   const blackjackCards = [ { rank: 1, suit: 0 }, { rank: 13, suit: 0 } ];
@@ -7,4 +7,11 @@ it('should calculate bust blackjack or live hand status', ()=>{
   expect( handStatus(blackjackCards).status ).toEqual( 'blackjack' );
 
   // etc.
+});
+
+it('should calculate the correct dealer action', ()=>{
+  const softSeventeen = [ { rank: 1, suit: 3 }, { rank: 6, suit: 2 } ];
+
+  expect( dealerStatus( softSeventeen ).total ).toEqual( 17 );
+  expect( dealerStatus( softSeventeen ).status ).toEqual( 'hitting' );
 });
