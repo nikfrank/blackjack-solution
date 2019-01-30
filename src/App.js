472a653978743ee048e5e4b15baa5eccd1a34b58
nik frank <nikbfrank@gmail.com>
2019-01-30 14:14:11 -0800

    hitting

--- src/App.js
+++ src/App.js
@@ -3,22 +3,41 @@ import './App.css';
 
 import { Hand } from 'react-deck-o-cards';
 
+import { handStatus } from './util';
+
 const newCard = ()=> ({ rank: Math.floor(Math.random()*13 + 1), suit: Math.floor(Math.random()*4) });
 
 class App extends Component {
   state = {
-    cards: [
-      { rank: 1, suit: 3 },
-      { rank: 13, suit: 3 },
-    ],
+    cards: [],
+    handStatus: 'live',
+    handTotal: 0,
   }
 
-  hit = ()=> this.setState(state=> ({ cards: [...state.cards, newCard()] }) )
+  hit = ()=> {
+    if( this.state.handStatus === 'bust' ) console.log('should remove the button on bust');
+    else this.dealCard();
+  }
+
+  dealCard = ()=>
+    this.setState(state => {
+      const nextCards = state.cards.concat(newCard());
+      const nextHand = handStatus(nextCards);
+      
+      return ({
+        cards: nextCards,
+        handStatus: nextHand.status, // we'll use this in the next task
+        handTotal: nextHand.total, // we can render this snp
+      });
+    })
+
   
   render() {
     return (
       <div className="App">
-        <Hand cards={this.state.cards}/>
+        <div className='hand'>
+          <Hand cards={this.state.cards}/>
+        </div>
         <button onClick={this.hit}>Hit me Jeeves</button>
       </div>
     );
