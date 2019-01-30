0c0aff9077d2a7d414cbefc0b2ab5f1bda4f802c
nik frank <nikbfrank@gmail.com>
2019-01-30 13:48:25 -0800

    hit me

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 import { Hand } from 'react-deck-o-cards';
 
+const newCard = ()=> ({ rank: Math.floor(Math.random()*13 + 1), suit: Math.floor(Math.random()*4) });
+
 class App extends Component {
   state = {
     cards: [
@@ -10,11 +12,14 @@ class App extends Component {
       { rank: 13, suit: 3 },
     ],
   }
+
+  hit = ()=> this.setState(state=> ({ cards: [...state.cards, newCard()] }) )
   
   render() {
     return (
       <div className="App">
         <Hand cards={this.state.cards}/>
+        <button onClick={this.hit}>Hit me Jeeves</button>
       </div>
     );
   }
